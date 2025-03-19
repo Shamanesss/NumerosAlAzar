96f171bd71b49d2ca12d52ffef3ffb861690dad4
Shamanesss <mentulbenodorna@gmail.com>
2025-03-19 15:48:01 +0100

    actualizado

--- NumerosAlAzar.php
+++ NumerosAlAzar.php
@@ -4,13 +4,13 @@
 
 <?php
 
-function azarEntre($desde, $hasta) {
+function azarEntre($num1, $num2) {
 
 srand(time());
 
-$cantidadCifras = $hasta - $desde + 1;
+$cantidadCifras = $num2 - $num1 + 1;
 
-$numAzar = (rand() % ($cantidadCifras)) + $desde;
+$numAzar = (rand() % ($cantidadCifras)) + $num1;
 
 return $numAzar;
 
@@ -20,7 +20,7 @@ echo "Un numero al azar entre 1 y 10 : Es ", azarEntre(1, 10), "<BR>";
 
 echo "Otro entre 5 y 7 : Es ", azarEntre(5, 7), "<br>";
 
-echo "y otro entre 100 y 200 : Es ", azarEntre(100, 200), "<BR>";
+echo "y otro entre 100 y 200 : Es ". azarEntre(100, 200). "<BR>";
 
 ?>
 
